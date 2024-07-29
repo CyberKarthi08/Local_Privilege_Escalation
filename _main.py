984099ec1087e6a41645c86b54401dbf102af9b8
Cyber_Karthi08 <karthikeyan9865602@gmail.com>
2024-07-29 13:17:39 +0530

    Initial commit

---
+++ _main.py
@@ -0,0 +1,16 @@
+
+
+class main_privilege_escalation():
+
+    def __init__(self) -> None:
+        pass
+
+    def check_operating_system():
+        print("")
+
+
+
+
+
+if __name__ == "__main__":
+
