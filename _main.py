17493954ce877a972088bfa007491a39f1709b39
Cyber_Karthi08 <karthikeyan9865602@gmail.com>
2024-08-06 13:34:16 +0530

    Demo

--- _main.py
+++ _main.py
@@ -2,19 +2,17 @@
 
 class main_privilege_escalation():
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
 
-    def check_operating_system():
-        print('''
-            
-            |######################################|
-            |   Linux Local Privilege Escalation   |
-            |######################################| 
-              
-              ''')
+        self.linux_01 = None
+        self.windows_02 = None
+
+    def check_operting_system():
+        print("\nTODO : Local Priviliege Eacalation\n")
+
 
 
 
 if __name__ == "__main__":
+    main_privilege_escalation.check_operting_system()
     
