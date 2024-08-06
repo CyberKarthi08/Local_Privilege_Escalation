d2c1e80e4618c7735d0ca7e631bbf48fb84520a4
Cyber_Karthi08 <karthikeyan9865602@gmail.com>
2024-08-06 15:10:47 +0530

    Demo

--- _main.py
+++ _main.py
@@ -1,6 +1,6 @@
+import linux_user_base
 
-
-class main_privilege_escalation():
+class privilege_escalation():
 
     def __init__(self):
 
@@ -8,11 +8,20 @@ class main_privilege_escalation():
         self.windows_02 = None
 
     def check_operting_system():
-        print("\nTODO : Local Priviliege Eacalation\n")
+        # TODO #1 --> If you want to checkout which operating system
+        print("\nTODO : linux Local Privilege Eacalation\n")
 
-
+class master_excution(linux_user_base.LinuxUserBase):
+    def __init__(self):
+        super().__init__()
+    
+    def main_excution():
+        linux_user_base.LinuxUserBase.check_user_privilege()
 
 
 if __name__ == "__main__":
-    main_privilege_escalation.check_operting_system()
+    privilege_escalation.check_operting_system()
+    master_excution.main_excution()
+    
+
     
--- linux_user_base.py
+++ linux_user_base.py
@@ -1,8 +1,24 @@
+import os 
+import pwd
+import logging
 import subprocess
+
+
 
 class LinuxUserBase():
 
     def __init__(self) -> None:
         pass
 
+    def check_user_privilege():
+        logging.warning("Hacked.....")
+        logging.info("Y")
+        that_uid = os.getuid()
+        that_user_name = pwd.getpwuid(that_uid).pw_name
+        print("UID : {0}" . format(that_uid))
+        print("USER_NAME : {0}" . format(that_user_name))
+
+
     
+
+
