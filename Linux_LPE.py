de9069673293db58259540bfadc0abd02a39b904
Cyber_Karthi08 <karthikeyan9865602@gmail.com>
2024-08-08 14:29:04 +0530

    Initial commit

--- Linux_LPE.py
+++ Linux_LPE.py
@@ -1,11 +1,11 @@
 import os 
 import pwd
 import logging
+import platform
 import subprocess
 
 
-
-class LinuxUserBase():
+class UserBase():
 
     def __init__(self) -> None:
         pass
@@ -18,7 +18,11 @@ class LinuxUserBase():
         print("UID : {0}" . format(that_uid))
         print("USER_NAME : {0}" . format(that_user_name))
 
+class KernelBase():
 
-    
+    def __init__(self) -> None:
+        pass
 
-
+    def kernel_info():
+        kernel_version = platform.release
+        print(f"Kernel Version  : {kernel_version}") 
